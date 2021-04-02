5833f0db37c9bd9516ef9cd793dc1603239b344c
Chetan <Chetan@LAPTOP-PCRP914C>
2021-04-02 22:07:31 +0530

    [Harshal] Adding project to Github Server

---
+++ src/Sample/Demo.java
@@ -0,0 +1,14 @@
+package Sample;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Work Hard to achieve your Goals");
+		
+		System.out.println("Wake up early in the morning");
+		
+		
+	}
+
+}
